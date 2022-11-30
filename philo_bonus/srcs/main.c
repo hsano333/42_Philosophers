4ffcb17381c8f5225e4260bf2566dda5dd0488b8
Hiroaki Sano <hsano@student.42tokyo.jp>
2022-11-30 14:27:03 +0900

    fixed boot time

--- philo_bonus/srcs/main.c
+++ philo_bonus/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hsano <hsano@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 21:32:05 by hsano             #+#    #+#             */
-/*   Updated: 2022/10/06 01:48:26 by hsano            ###   ########.fr       */
+/*   Updated: 2022/11/30 14:25:29 by hsano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,14 @@
 int	main(int argc, char **argv)
 {
 	t_philos	*philos;
+	t_time		boot_time;
 
 	philos = init_philos(argc, argv);
 	if (!philos)
 		return (1);
 	create_notice_process(philos);
+	gettimeofday(&boot_time, NULL);
+	philos->boot_time = boot_time;
 	create_thread_for_process(philos);
 	clear_all(philos);
 	return (0);
